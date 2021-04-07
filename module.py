3fce6227acd682590d4ed17700a1dd6673f4df19
Halim Madjid <halimmadjid@gmail.com>
2021-04-06 21:03:32 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,12 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+import math
+
+def square_foot(length, width):
+    area = length * width
+    return area
+
+def circumference(r):
+    circ = 2*math.pi*r
+    return circ 
